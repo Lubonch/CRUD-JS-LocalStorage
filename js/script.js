c1c529a0cd3c40ce853d493d6cd758738f225843
Lucas Eduardo Ramirez Bonch <39447963+Lubonch@users.noreply.github.com>
2020-11-06 18:06:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,32 +3,37 @@ const listProductos = document.getElementById('listaProductos');
 const elimTodo = document.querySelector('#eliminartodo');
 const buscar = document.querySelector('#buscar');
 let arrayProductos = [];
+let lastId = 0;
 
+//functions
 
-const CrearProducto = (producto, cantidad, precio) => { //Esto crea Productos
+const CreateProduct = (idprod, producto, cantidad, precio) => { //Esto crea Productos
 
     let item = {
+      id: idprod,        
       producto: producto,
       cantidad: cantidad,
       precio: precio
     }
     arrayProductos = arrayProductos || [];
     arrayProductos.push(item);
-  
+    SetIdOnls();
     return item;
 }
 
-const AltaLS = () => { //carga el arrar de productos en el LS
+const LoadLS = () => { //carga el arrar de productos en el LS
 
     localStorage.setItem('productos', JSON.stringify(arrayProductos));
   
-    LeerLS();
+    ReadLS();
   
   }
 
-const LeerLS = () => { //se encarga de leer el Ls y cargarlo en el html
+const ReadLS = () => { //se encarga de leer el Ls y cargarlo en el html
     Loadtablehead();
     LStoArray();
+
+    LoadID();
 
     if(arrayProductos != null){
         arrayProductos.forEach(element => {
@@ -37,41 +42,40 @@ const LeerLS = () => { //se encarga de leer el Ls y cargarlo en el html
     }
 }
 
-const BajaLS = (producto) =>{ //se encarga de dar de baja un producto
-    let arrayIndex = arrayProductos.findIndex((e) => e.producto === producto);
+const DeleteLS = (idproducto) =>{ //se encarga de dar de baja un producto
+    let arrayIndex = FindArrayIndex(idproducto);
     arrayProductos.splice(arrayIndex, 1);
-    AltaLS();
+    LoadLS();
 }
 
-const ModifLS = (producto) =>{ //me permite modificar un producto
-    let arrayIndex = arrayProductos.findIndex((e) => e.producto === producto);//busco el indice del producto que quiero modificar
+const ModifLS = (idproducto) =>{ //me permite modificar un producto
+    let arrayIndex = FindArrayIndex(idproducto);//busco el indice del producto que quiero modificar
     let modnom = window.prompt("Producto: ");
     let modcant = window.prompt("Cantidad: ");
     let modpre = window.prompt("Precio: ");
 
-    if(modnom != null) arrayProductos[arrayIndex].producto = modnom;
+    if(modnom != null && modnom != 0) arrayProductos[arrayIndex].producto = modnom;
 
-    if(modcant != null) arrayProductos[arrayIndex].cantidad = modcant;
+    if(modcant != null && modcant != 0) arrayProductos[arrayIndex].cantidad = modcant;
 
-    if(modpre != null) arrayProductos[arrayIndex].precio = modpre;
+    if(modpre != null && modpre != 0) arrayProductos[arrayIndex].precio = modpre;
     
-    AltaLS();
+    LoadLS();
 }
 
-const EliminarLS = () => { //elimina todo el localstorage
-    localStorage.clear();
+const DeleteAllLS = () => { //elimina todo el localstorage
+    localStorage.removeItem('productos');
     LStoArray();
-    AltaLS();
+    LoadLS();
 }
 
-const BuscarLS = (searchvalue) =>{ //se encarga de buscar cosas dentro del LS
+const SearchLS = (searchvalue) =>{ //se encarga de buscar cosas dentro del LS
     //alert("En construcción ¯\\\_(ツ)_/¯");
     Loadtablehead();
 
     if(arrayProductos != null){
         arrayProductos.forEach(element => {
                 if(element.producto.startsWith(searchvalue)){ //itero sobre el array de objetos buscando los elementos donde el nombre de producto inicie con el string de la caja de busqueda
-                    console.log("Encontre! "+element.producto);
                     Loadtablecontent(element);
                 }
         }   );
@@ -82,6 +86,7 @@ const BuscarLS = (searchvalue) =>{ //se encarga de buscar cosas dentro del LS
 const Loadtablehead = () => {
     listProductos.innerHTML = `<table>
                                 <tr>
+                                    <th class="column-0">ID</th>
                                     <th class="column-1">Producto</th>
                                     <th class="column-2">Cantidad</th>
                                     <th class="column-3">Precio</th>
@@ -94,6 +99,7 @@ const Loadtablecontent = (element) => {
     `<div class="alert alert-danger" role="alert">
         <table>
             <tr>
+                <td class="column-0"><b>${element.id}</b></td>
                 <td class="column-1"><b>${element.producto}</b></td>
                 <td class="column-2"><b>${element.cantidad}</b></td>
                 <td class="column-3">$<b>${element.precio}</b></td>
@@ -111,39 +117,52 @@ const Loadtablecontent = (element) => {
 const LStoArray = () =>{
     arrayProductos = JSON.parse(localStorage.getItem('productos')|| "[]");
 }
-elimTodo.addEventListener('submit', EliminarLS);
+
+const SetIdOnls = () =>{
+    localStorage.setItem('lastid', lastId);
+}
+
+const LoadID = () =>{
+    lastId = localStorage.getItem('lastid') || 0;
+}
+
+const FindArrayIndex = (value) =>{
+    return  arrayProductos.findIndex((e) => e.id == value);
+}
+
+//Events listeners
+
+elimTodo.addEventListener('submit', DeleteAllLS);
 
 formulario.addEventListener('submit', (item) => {
         item.preventDefault();
-
+        lastId++;
         let productoNom = document.querySelector('#nombreProducto').value;
         let productoCant = document.querySelector('#cantidad').value;
         let productoPrice = document.querySelector('#precio').value;
 
-        CrearProducto(productoNom, productoCant, productoPrice);
-        AltaLS();
+        CreateProduct(lastId, productoNom, productoCant, productoPrice);
+        LoadLS();
 
         formulario.reset();
         
     });
 
-document.addEventListener('DOMContentLoaded', LeerLS)
+document.addEventListener('DOMContentLoaded', ReadLS)
 
 listProductos.addEventListener('click', (e) => {
     e.preventDefault();
     const valor = e.path[3].childNodes[1].childNodes[0].innerHTML;
-    console.log(valor);
     
     if(e.target.innerHTML === 'create'){
         ModifLS(valor);
     }else if (e.target.innerHTML === 'delete_forever'){
-        BajaLS(valor);
+        DeleterLS(valor);
     }
 })
 
 buscar.addEventListener('submit', (e) => {
     e.preventDefault();
     e = document.getElementById("busqueda").value;
-    console.log("Quiero encontrar: "+e);
-    BuscarLS(e);
+    SearchLS(e);
 });
